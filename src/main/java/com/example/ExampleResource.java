e3331863515601e11377f005c62298f11f44c8f1
Nishchal Tiwari <nishchal98765@gmail.com>
2021-11-16 22:51:24 +0530

    Initial commit

---
+++ src/main/java/com/example/ExampleResource.java
@@ -0,0 +1,7 @@
+package com.example;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
---
+++ src/main/java/com/example/thread.java
@@ -0,0 +1,10 @@
+package com.example;
+class a{
+
+}
+class b{
+
+}
+class c{
+    
+}
